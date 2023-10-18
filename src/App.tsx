dfbce1bcf7deeb99537cf5d3a11186364363cb1c
0xForkHunter <0xforkhunter@protonmail.com>
2023-10-18 21:01:49 +0200

    Add link to talent protocol. Fix display issues. Wait for fonts load

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,29 @@ import logo from "./assets/logo.svg";
 import logoWhite from "./assets/logo-white.svg";
 import backLogo from "./assets/back-logo.svg";
 import waitlist from "@zootools/waitlist-js";
+import { useEffect, useState } from "react";
+import FontFaceObserver from "fontfaceobserver";
 
 const waitlistId = "FiWQu6gi2lHXsJaskHgA";
 
 function App() {
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+  const [isReady, setIsReady] = useState(false);
+
   const joinWaitlist = async () => {
     waitlist.openPopup(waitlistId);
   };
+
+  useEffect(() => {
+    (async () => {
+      await new FontFaceObserver("Mona-Sans").load();
+      await new FontFaceObserver("Space Grotesk").load();
+      setIsReady(true);
+    })();
+  }, []);
+
+  if (!isReady) return <></>;
+
   return (
     <div id="container">
       <div className="desktop topbar">
@@ -31,23 +47,30 @@ function App() {
         </a>
 
         {/* Hamburger menu */}
-        <div className="menu-btn" onClick={(e) => e.currentTarget.classList.toggle("active")}>
-          <div className="btn-line"></div>
-          <div className="btn-line"></div>
+        <div className="mobile menu-btn" onClick={() => setSidebarOpen((open) => !open)}>
+          <div
+            style={{ transform: sidebarOpen ? "translateY(4px) rotate(45deg)" : undefined }}
+            className="btn-line"
+          ></div>
+          <div
+            style={{ transform: sidebarOpen ? "translateY(-4px) rotate(-45deg)" : undefined }}
+            className="btn-line"
+          ></div>
         </div>
-        <div className="mobile-menu">
-          <ul>
-            <li>
-              <a href="https://twitter.com/BuilderFi">Follow us</a>
-            </li>
-            <li>
-              <a href="#">FAQ</a>
-            </li>
-          </ul>
-          <div style={{ display: "flex", flexDirection: "column", rowGap: "34px", padding: "0px 16px" }}>
-            <div id="join-waitlist" onClick={joinWaitlist}>
-              Join Waitlist
-            </div>
+      </div>
+
+      <div className="mobile-menu" style={{ right: sidebarOpen ? 0 : undefined }}>
+        <ul>
+          <li>
+            <a href="https://twitter.com/BuilderFi">Follow us</a>
+          </li>
+          <li>
+            <a href="#">FAQ</a>
+          </li>
+        </ul>
+        <div style={{ display: "flex", flexDirection: "column", rowGap: "34px", padding: "0px 16px" }}>
+          <div id="join-waitlist" onClick={joinWaitlist}>
+            Join Waitlist
           </div>
         </div>
       </div>
@@ -63,7 +86,15 @@ function App() {
       </div>
 
       <div className="bottom-bar">
-        <span className="bottom-bar-text">From the creators of Talent Protocol</span>
+        <span className="bottom-bar-text">
+          From the creators of{" "}
+          <a
+            href={"https://www.talentprotocol.com/"}
+            style={{ textDecoration: "none", fontWeight: 700, cursor: "pointer" }}
+          >
+            Talent Protocol
+          </a>
+        </span>
         <span className="bottom-bar-text">Launching soon on Base</span>
       </div>
     </div>
