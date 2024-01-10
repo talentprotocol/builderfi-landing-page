2f9a85d61d28c8a5eed1679faca29a2b6862c87c
francisco-leal <francisco@talentprotocol.com>
2024-01-10 15:32:34 +0000

    reintroduce FAQ replace

--- src/routes/faq.tsx
+++ src/routes/faq.tsx
@@ -1,4 +1,10 @@
+import { useEffect } from "react";
+
 export default function FAQ() {
+  useEffect(() => {
+    window.location.replace("https://www.notion.so/talentprotocol/builder-fi-FAQ-dcebfe7103b34d11aba90de032173b39");
+  }, [])
+
   return (
     <div
       style={{
